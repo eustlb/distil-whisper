bd05782cf1457ef65e54aee3d01ac1ad74c3ae5d
Eustache Le Bihan <eulebihan@gmail.com>
2024-04-13 16:32:31 +0000

    typo in arg help

--- training/create_student_model.py
+++ training/create_student_model.py
@@ -63,7 +63,7 @@ def parse_args():
         "--decoder_layers_numbers",
         type=int,
         nargs="*",
-        help="Layerd numberd of the decoder teacher to use in the student model. Defaults to first and last layer (equiv. 0 -1)",
+        help="Layers numbers of the decoder teacher to use in the student model. Defaults to first and last layer (equiv. 0 -1)",
     )
     parser.add_argument(
         "--save_dir",
